bf59cf9b083ce55618ae2746c06dc24ad26f6c5b
Rafael Giusti <rgiusti@huhsp.org.br>
2016-09-23 15:32:55 -0300

    Bug corrected

--- page2.php
+++ page2.php
@@ -75,11 +75,10 @@ if(isset($_SESSION['access_token']) && $_SESSION['access_token']) {
     } 
     catch (Google_Service_Exception $e) {
         $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
-        htmlspecialchars($e->getMessage()));
+        htmlspecialchars($e->getMessage()).' '.$e->getTraceAsString());
     } catch (Google_Exception $e) {
         $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
-        htmlspecialchars($e->getMessage()));
-        print_r($e);
+        htmlspecialchars($e->getMessage()).' '.$e->getTraceAsString());
     }
 
 } else {
